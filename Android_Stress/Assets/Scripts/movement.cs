537e0825fbc3e870e0284d5d52a53c9aa9a669b6
KingOfFawns <fister>
2017-02-13 14:06:27 +0100

    Basic_Structure_Commit1
    Sphere structures, basis physics, limited area, touch behavior basis

---
+++ Android_Stress/Assets/Scripts/movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	RaycastHit2D hit;
+
+	Vector3 direction;
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		direction = new Vector3 (Random.Range(-1f,1f), Random.Range(-1f,1f), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector3.Lerp(transform.position, transform.position + direction, speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		Debug.Log (other.gameObject.name);
+
+		if (other.gameObject.name == "leftWall") {
+			direction = Vector3.Reflect (direction, Vector3.right);
+		}
+		if (other.gameObject.name == "rightWall") {
+			direction = Vector3.Reflect (direction, Vector3.left);
+		}
+		if (other.gameObject.name == "topWall") {
+			direction = Vector3.Reflect (direction, Vector3.down);
+		}
+		if (other.gameObject.name == "bottomWall") {
+			direction = Vector3.Reflect (direction, Vector3.up);
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+
+		ContactPoint con = col.contacts[0];
+
+		direction = Vector3.Reflect (direction, con.normal); 
+
+	}
+}
---
+++ Android_Stress/Assets/Scripts/touchTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchTest : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit hit;
+	bool colorChange = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+			ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+			Debug.DrawRay (ray.origin, ray.direction * 20f, Color.red);
+
+			if (Physics.Raycast (ray,out hit,Mathf.Infinity)) {
+				Debug.Log ("HIT");
+
+				if (hit.collider.tag == "Special") {
+					if (colorChange) {
+						hit.transform.gameObject.GetComponent<Renderer> ().material.SetColor ("_Color", Color.blue);
+						colorChange = false;
+					} else {
+						hit.transform.gameObject.GetComponent<Renderer> ().material.SetColor ("_Color", Color.red); 
+						colorChange = true;
+					}
+				}
+			}
+		}
+	}
+}
